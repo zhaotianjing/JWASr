06df05b8e3bfe2cd1122dccb7327f0c0153ea86d
zhaotianjing <tjzhao@ucdavis.edu>
2018-08-26 21:15:50 -0700

    Delete example.jl

--- example.jl
+++
@@ -1,7 +0,0 @@
-
-using CSV,JWAS
-phenotypes = CSV.read("phenotypes.txt",delim = ',',header=true)
-model_equation = "y1 = intercept + x1"
-R=1.0
-model=build_model(model_equation, R)
-out=runMCMC(model,phenotypes)
