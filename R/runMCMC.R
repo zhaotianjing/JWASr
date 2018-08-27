c45fdeac01c071ed2c962aaccf42978e266f2268
zhaotianjing <tjzhao@ucdavis.edu>
2018-08-27 16:50:59 -0700

    edit runMCMC

--- R/runMCMC.R
+++ R/runMCMC.R
@@ -11,15 +11,19 @@
 #'
 #' @export
 
-runMCMC = function(model, mydata,chain_length=1000,output_samples_frequency=0, outputEBV = 0){
+runMCMC = function(model, mydata, estimatePi = 0, chain_length = 1000, methods="conventional (no markers)",
+                   output_samples_frequency = 0, outputEBV = 0){
   JuliaCall::julia_assign("model", model)
   JuliaCall::julia_assign("mydata", mydata)
+  JuliaCall::julia_assign("estimatePi", estimatePi)
   JuliaCall::julia_assign("chain_length", chain_length)
+  JuliaCall::julia_assign("methods",methods)
   JuliaCall::julia_assign("output_samples_frequency", output_samples_frequency)
   JuliaCall::julia_command("output_samples_frequency = Int(output_samples_frequency)")
   JuliaCall::julia_assign("outputEBV", outputEBV)
 
-  JuliaCall::julia_command("out = runMCMC(model, mydata,chain_length=chain_length,output_samples_frequency=output_samples_frequency,
+
+  JuliaCall::julia_command("out = runMCMC(model, mydata, estimatePi = estimatePi, chain_length=chain_length,methods = methods, output_samples_frequency=output_samples_frequency,
                            outputEBV = outputEBV)")
   out=JuliaCall::julia_eval("out")
   return(out)
