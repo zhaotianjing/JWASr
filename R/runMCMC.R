4c2bb6b3e2201ddc9126f991d2ab331958b6f394
zhaotianjing <tjzhao@ucdavis.edu>
2018-08-26 22:36:57 -0700

    JWASr

--- R/runMCMC.R
+++ R/runMCMC.R
@@ -18,7 +18,7 @@ runMCMC = function(model, mydata,chain_length=1000,output_samples_frequency=0){
   JuliaCall::julia_assign("output_samples_frequency", output_samples_frequency)
   JuliaCall::julia_command("output_samples_frequency = Int(output_samples_frequency)")
 
-  JuliaCall::julia_command("out = runMCMC(model, mydata,chain_length=chain_length,output_samples_frequency=output_samples_frequency);")
+  JuliaCall::julia_command("out = runMCMC(model, mydata,chain_length=chain_length,output_samples_frequency=output_samples_frequency)")
   out=JuliaCall::julia_eval("out")
   return(out)
 }
