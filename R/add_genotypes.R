23dfe30c3c8a5b8a9ae6758e1b942d2759ee63e1
zhaotianjing <tjzhao@ucdavis.edu>
2018-08-27 21:38:30 -0700

    edit add_genotypes

--- R/add_genotypes.R
+++ R/add_genotypes.R
@@ -3,13 +3,11 @@
 #' @export
 
 
-add_genotypes = function(path, G3){
+add_genotypes = function(path, G_geno, separator = ','){
   JuliaCall::julia_assign("genofile", path)
-  JuliaCall::julia_assign("G3", G3)
-  
-
-  
-  JuliaCall::julia_command("add_genotypes(model, genofile, G, separator = ',')")
+  JuliaCall::julia_assign("G_geno", G_geno)
+  JuliaCall::julia_assign("separator", separator)
+  JuliaCall::julia_command("add_genotypes(model, genofile, G_geno, separator = separator)")
 }
 
   
